fc4684eee6e0236a7b6c2897b86a42db8e6779fc
Abdullah Almariah <abdullahalmariah@gmail.com>
2020-07-04 17:36:18 +0200

    init

--- hack/build-mac.sh
+++ hack/build-mac.sh
@@ -25,3 +25,21 @@ cp -rf build/qr-secret-darwin-amd64 build/QRSecret/QRSecret.app/Contents/MacOS/q
 cp -rf logo/icon.icns build/QRSecret/QRSecret.app/Contents/Resources
 
 hdiutil create build/QRSecret.dmg -srcfolder build/QRSecret -ov
+
+#export CERTIFICATE_P12=Certificate.p12;
+#echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12;
+#export KEYCHAIN=build.keychain;
+#security create-keychain -p mysecretpassword $KEYCHAIN;
+#security default-keychain -s $KEYCHAIN;
+#security unlock-keychain -p mysecretpassword $KEYCHAIN;
+#security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+
+######
+#security create-keychain -p mysecretpassword build.keychain
+#security default-keychain -s build.keychain
+#security unlock-keychain -p mysecretpassword build.keychain
+#security import certificate.p12 -k build.keychain -P <certificate password, if any> -T /usr/bin/codesign
+#security find-identity -v
+####
+
+#codesign --deep --force --verbose --sign "<identity>" Application.app
